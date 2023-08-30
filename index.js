5ae173c5014e852513c838477852ea085864d60d
EstherAdegbaju <princessadeiye@gmail.com>
2023-08-31 00:20:52 +0200

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// code your solution here
+function superbowlWin(array) {
+    let result = array.find((obj) => { return obj.result === "W" })
+    if (result) {
+        return result.year
+    } else {
+        return undefined
+    }
+}
